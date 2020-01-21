ab828b7527719351f9a1ef81e48af82704c1d062
Franchell Polanco <polanco.franchell@gmail.com>
2020-01-21 06:24:13 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,11 @@
-def square_array(array)
-  # your code here
+# def square_array(array)
+#    new_numbers = array.new(array.length)
+#    array.each_index { |i| new_numbers[i] =array[i] * array[i] }
+#    puts new_numbers
+# end
+
+def square_array(ary)
+  a = Array.new(ary.length)
+  ary.each_index { |i| a[i] = ary[i] * ary[i] }
+  a
 end
